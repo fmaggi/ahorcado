8def47d6fa5638b0313b8e30b166cff60eb158e9
fmaggi <franciscomaggi1999@gmail.com>
2021-06-03 14:56:12 -0300

    Constructor de diccionario implementado

--- src/diccionario.cpp
+++ src/diccionario.cpp
@@ -1,11 +1,37 @@
 #include "diccionario.h"
 #include <iostream>
+#include <fstream>
+#include <ctime>
 
 using namespace std;
 
 Diccionario::Diccionario(int nivel)
-{
-    m_palabra = "palabra";
+{   
+    char path[] = {"res/dicN.txt"};
+    char n = '0' + nivel;
+    path[7] = n;
+
+    ifstream dic(path);
+    if (!dic.is_open())
+    {   
+        dic.close();
+        cout << "No se pudo abrir el diccionario" << endl;
+        return;
+    }
+
+    // Primera linea del diccionario te dice cuantas palabras hay en el diccionario
+    int numberOfWords;
+    dic >> numberOfWords;
+    srand(time(0));
+    int index = rand() % numberOfWords;
+
+    for (int i = 0; i < index; i++)
+    {
+        if (dic.eof())
+            break;
+        dic >> m_palabra;
+    }
+    dic.close();
 }
 
 void Diccionario::agregarPalabra()
